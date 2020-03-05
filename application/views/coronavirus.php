4c8c8f7210bbc9acb85632389b927f8c019b88b4
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-03-05 17:21:42 +0300

    Update coronavirus.php

--- application/views/coronavirus.php
+++ application/views/coronavirus.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
@@ -65,11 +65,12 @@
         <div class="row">
             <div class="col-9"></div>
             <div class="col-3">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" id="search">
+                <div class="form-group mb-4">
+                    <input type="text" placeholder="Search" class="form-control border-dark" id="search">
+                </div>
             </div>
         </div>
 
-        <br>
         <table class="table">
             <thead>
                 <tr>
