80dd3f80b6af8e4ca43d2405c4536c40aad4da15
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-03-12 02:08:54 +0300

    TotalDeath and TotalRecovered added.

--- application/controllers/CoronaVirusData.php
+++ application/controllers/CoronaVirusData.php
@@ -6,21 +6,32 @@ class CoronaVirusData extends CI_Controller
     public function index()
     {
         $url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+        $url2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
+        $url3 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+
         $contents = file_get_contents($url);
+        $contents2 = file_get_contents($url2);
+        $contents3 = file_get_contents($url3);
         if ($contents === false) {
             $result['error'] = true;
             $this->load->view('coronavirus',$result);
         }
         else{
-            $csv = array_map('str_getcsv', explode("\n", $contents));
-            $info = $csv[0];
-            unset($csv[0]);
+            $csvConfirmed = array_map('str_getcsv', explode("\n", $contents));
+            $csvDeaths = array_map('str_getcsv', explode("\n", $contents2));
+            $csvRecovered = array_map('str_getcsv', explode("\n", $contents3));
+            $info = $csvConfirmed[0];
+            unset($csvConfirmed[0]);
+            unset($csvDeaths[0]);
+            unset($csvRecovered[0]);
             $sonuc['results'] = array();
             $sonuc['TOTAL_CASE_REPORTED'] = 0;
+            $sonuc['TOTAL_DEATHS_REPORTED'] = 0;
+            $sonuc['TOTAL_RECOVERED_REPORTED'] = 0;
             $sonuc['TOTAL_CASE_REPORTED_LASTDAY'] = 0;
 
     
-            foreach($csv as $row => $innerArray){
+            foreach($csvConfirmed as $row => $innerArray){
                 if(count($innerArray) == count($info)){
                     $sonuc['results'][] = array(
                         "STATE" => $innerArray[0],
@@ -32,7 +43,28 @@ class CoronaVirusData extends CI_Controller
                     $sonuc['TOTAL_CASE_REPORTED_LASTDAY'] += $innerArray[count($innerArray)-2]!= NULL ? $innerArray[count($innerArray)-2]:0;
                 }  
             }
-            $this->load->view('coronavirus',$sonuc);
+
+          $i = 0;
+            foreach($csvDeaths as $row => $innerArray){
+                if(count($innerArray) == count($info)){
+                    $sonuc['results'][$i++]["TOTAL_DEATH"] = $innerArray[count($innerArray)-1];
+                    $sonuc["TOTAL_DEATHS_REPORTED"] += $innerArray[count($innerArray)-1] != NULL ? $innerArray[count($innerArray)-1]:0;
+                }  
+            }
+
+            $i = 0;
+            foreach($csvRecovered as $row => $innerArray){
+                if(count($innerArray) == count($info)){
+                    $sonuc['results'][$i++]["TOTAL_RECOVERED"] = $innerArray[count($innerArray)-1];
+                    $sonuc["TOTAL_RECOVERED_REPORTED"] += $innerArray[count($innerArray)-1] != NULL ? $innerArray[count($innerArray)-1]:0;
+                }  
+            }
+
+           
+            // echo "<pre>"; print_r($sonuc); echo "</pre>";
+          $this->load->view('coronavirus',$sonuc);
         }
     }
 }
+
+
