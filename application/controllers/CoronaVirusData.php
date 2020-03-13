1e87267413d20b2183e430be88567d887d071180
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-03-13 17:02:35 +0300

    Update CoronaVirusData.php

--- application/controllers/CoronaVirusData.php
+++ application/controllers/CoronaVirusData.php
@@ -3,68 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CoronaVirusData extends CI_Controller 
 {
+    private $url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-";
+    private $csvColumns =  array("Confirmed","Deaths","Recovered");
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
     public function index()
     {
-        $url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
-        $url2 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
-        $url3 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+        $urlCsvColumns;
 
-        $contents = file_get_contents($url);
-        $contents2 = file_get_contents($url2);
-        $contents3 = file_get_contents($url3);
-        if ($contents === false) {
-            $result['error'] = true;
-            $this->load->view('coronavirus',$result);
+        foreach($this->csvColumns as $each){
+            $sonuc = $this->url.$each;
+            $urlCsvColumns[] = array($sonuc);
         }
-        else{
-            $csvConfirmed = array_map('str_getcsv', explode("\n", $contents));
-            $csvDeaths = array_map('str_getcsv', explode("\n", $contents2));
-            $csvRecovered = array_map('str_getcsv', explode("\n", $contents3));
-            $info = $csvConfirmed[0];
-            unset($csvConfirmed[0]);
-            unset($csvDeaths[0]);
-            unset($csvRecovered[0]);
-            $sonuc['results'] = array();
-            $sonuc['TOTAL_CASE_REPORTED'] = 0;
-            $sonuc['TOTAL_DEATHS_REPORTED'] = 0;
-            $sonuc['TOTAL_RECOVERED_REPORTED'] = 0;
-            $sonuc['TOTAL_CASE_REPORTED_LASTDAY'] = 0;
+        
+        $instance = new CoronaVirus($urlCsvColumns,$this->csvColumns);
+        $instance->fetchData();
+        $result = $instance->processCsv();
 
-    
-            foreach($csvConfirmed as $row => $innerArray){
-                if(count($innerArray) == count($info)){
-                    $sonuc['results'][] = array(
-                        "STATE" => $innerArray[0],
-                        "COUNTRY" => $innerArray[1],
-                        "TOTAL_CASE" => $innerArray[count($innerArray)-1],
-                        "TOTAL_CASE_LASTDAY" => $innerArray[count($innerArray)-2]
-                    );
-                    $sonuc['TOTAL_CASE_REPORTED'] += $innerArray[count($innerArray)-1] != NULL ? $innerArray[count($innerArray)-1]:0;
-                    $sonuc['TOTAL_CASE_REPORTED_LASTDAY'] += $innerArray[count($innerArray)-2]!= NULL ? $innerArray[count($innerArray)-2]:0;
-                }  
-            }
-
-          $i = 0;
-            foreach($csvDeaths as $row => $innerArray){
-                if(count($innerArray) == count($info)){
-                    $sonuc['results'][$i++]["TOTAL_DEATH"] = $innerArray[count($innerArray)-1];
-                    $sonuc["TOTAL_DEATHS_REPORTED"] += $innerArray[count($innerArray)-1] != NULL ? $innerArray[count($innerArray)-1]:0;
-                }  
-            }
-
-            $i = 0;
-            foreach($csvRecovered as $row => $innerArray){
-                if(count($innerArray) == count($info)){
-                    $sonuc['results'][$i++]["TOTAL_RECOVERED"] = $innerArray[count($innerArray)-1];
-                    $sonuc["TOTAL_RECOVERED_REPORTED"] += $innerArray[count($innerArray)-1] != NULL ? $innerArray[count($innerArray)-1]:0;
-                }  
-            }
-
-           
-            // echo "<pre>"; print_r($sonuc); echo "</pre>";
-          $this->load->view('coronavirus',$sonuc);
-        }
+        $this->load->view('coronavirus',$result);
     }
 }
-
-
