8d52866c51e02efe6b8456070965885d01d03f24
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2020-04-05 02:35:09 +0300

    Create footer_vw.php

---
+++ application/views/template/footer_vw.php
@@ -0,0 +1,29 @@
+                            <div class="row ">
+                                <div class=" col-lg-12">
+                                    <div class="page-title footerchange">© 2020 Coronavirus Tracker - Furkan KAHVECİ
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+<?php $this->load->view("template/js_vw");?>
+
+<script>
+    $(document).ready(function () {
+        $('table').DataTable({
+            order: [],
+            paging: true,
+            "pageLength": 25,
+            "pagingType": "full_numbers",
+            "dom": '<"top"flp>rt<"bottom"ip><"clear">',
+        });
+    });
+</script>
