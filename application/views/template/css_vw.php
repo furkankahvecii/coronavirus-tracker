8b75df4415a2be1bf388df5df2391a0e916e24b0
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2020-04-05 02:34:45 +0300

    Create css_vw.php

---
+++ application/views/template/css_vw.php
@@ -0,0 +1,8 @@
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url();?>assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url();?>assets/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url();?>assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url();?>assets/plugins/datatables/fixedHeader.bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url();?>assets/plugins/datatables/responsive.bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url();?>assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
