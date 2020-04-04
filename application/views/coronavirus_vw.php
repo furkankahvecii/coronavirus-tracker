30bce1fa100a66d6d5de86b6a339603d0551a801
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2020-04-05 02:33:14 +0300

    New system with views.

--- application/views/coronavirus_vw.php
+++ application/views/coronavirus_vw.php
@@ -1,68 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="description" content="Corona Virus Tracker by Furkan KAHVECİ">
-    <meta name="keywords" content="Corona, COVID-19, Coronavirus, Coronavirus Tracker">
-    <meta name="author" content="Furkan KAHVECİ">
-    <meta http-equiv="X-UA-CompatibBle" content="IE=edge" />
-
-    <title>Coronavirus Map</title>
-    <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico">
-
-    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url();?>assets/css/icons.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url();?>assets/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url();?>assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url();?>assets/plugins/datatables/fixedHeader.bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url();?>assets/plugins/datatables/responsive.bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url();?>assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-    <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['geochart'],
-            // Note: you will need to get a mapsApiKey for your project.
-            // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-            'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-        });
-        google.charts.setOnLoadCallback(drawRegionsMap);
-
-        function drawRegionsMap() {
-            var data = google.visualization.arrayToDataTable([
-                ['Country', 'Confirmed Cases', ], <?php foreach($RESULT_DATA as $map) {
-                    echo '["'.$map['country'].
-                    '",'.$map['cases'].
-                    '],';
-                } ?>
-            ]);
-
-            var options = {
-                colorAxis: {
-                    colors: ['#D2E3FC', '#8AB4F8', '#4285F4', '#1967D2', '#174EA6', ]
-                },
-                backgroundColor: '#2A323C',
-                legend: {
-                    textStyle: {
-                        color: '#000',
-                        fontSize: 14
-                    }
-                }
-            };
-
-            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
-            chart.draw(data, options);
-        }
-    </script>
-
-</head>
-
-
+<?php $this->load->view("template/head_vw");?>
+<?php $this->load->view("geochart_vw");?>
 <body>
-
     <div id="content-page">
         <div class="content">
             <div class="row ">
@@ -71,7 +9,6 @@
                         <h4 class="page-title" style="text-align:center;">Coronavirus Tracker Application</h4>
                     </div>
                 </div>
-
             </div>
             <div class="page-content-wrapper ">
                 <div class="container-fluid">
@@ -148,14 +85,11 @@
                                     <div class="row">
                                         <div class="col-12">
                                             <div class="table-responsive">
-                                                <table id="datatable"
-                                                    class="table table-striped table-bordered dt-responsive nowrap"
-                                                    cellspacing="0" width="100%">
+                                                <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                                                     <thead>
                                                         <tr>
                                                             <th>Location</th>
                                                             <th>Confirmed cases</th>
-                                                        <!--<th>Cases per 1M people</th>-->
                                                             <th>Deaths</th>
                                                             <th>Recovered</th>
                                                             <th>Total Tests</th>
@@ -164,14 +98,11 @@
                                                     </thead>
                                                     <tbody>
                                                         <?php foreach($RESULT_DATA as $result){ ?>
-                                                        <tr>
-                                                            
-                                                            <td><img src="<?php if($result['country'] == "World") 
-                                                            {echo $result['image'];}
+                                                        <tr>           
+                                                            <td><img src="<?php if(strpos($result['image'], 'gstatic') !== false) {echo $result['image'];}
                                                             else{ echo "https://www.gstatic.com/onebox/sports/logos/flags/".strtolower(str_replace(" ","_",$result['image']))."_icon_square.svg" ;}?>
                                                             " height="20" width="20"> <?php echo $result['country'];?></td>
                                                             <td><?php echo number_format($result['cases']);?></td>
-                                                        <!--<td><?php echo number_format($result['casesPerOneMillion']);?></td>-->
                                                             <td><?php echo number_format($result['deaths']);?></td>
                                                             <td><?php echo number_format($result['recovered']);?></td>
                                                             <td><?php echo number_format($result['totalTests']);?></td>
@@ -185,50 +116,4 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="row ">
-                                <div class=" col-lg-12">
-                                    <div class="page-title footerchange">© 2020 Coronavirus Tracker - Furkan KAHVECİ
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-
-    </div>
-    </div>
-
-    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
-    <script src="<?php echo base_url();?>assets/js/popper.min.js"></script>
-    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
-
-    <!-- Required datatable js-->
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js">
-    </script>
-    <script src="<?php echo base_url();?>assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Responsive examples -->
-    <script src="<?php echo base_url();?>assets/plugins/datatables/dataTables.responsive.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
-
-    <script>
-        $(document).ready(function () {
-            $('table').DataTable({
-                order: [],
-                paging: true,
-                "pageLength": 25,
-                "pagingType": "full_numbers",
-                "dom": '<"top"flp>rt<"bottom"ip><"clear">',
-            });
-        });
-    </script>
-</body>
-
-</html>
+                            <?php $this->load->view("template/footer_vw");?>
