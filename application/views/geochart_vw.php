9fd0974afa67b758e524600ee7ad8413d13780aa
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2020-04-05 02:34:20 +0300

    Create geochart_vw.php

---
+++ application/views/geochart_vw.php
@@ -0,0 +1,27 @@
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['geochart'],
+            'mapsApiKey': 'AIzaSyCQTRyg4qkOoIUkWZ-QWLU3VOrF4NqgPrY'
+        });
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        function drawRegionsMap() {
+            var data = google.visualization.arrayToDataTable([
+                ['Country', 'Confirmed Cases', ], <?php foreach($RESULT_DATA as $map) {
+                    echo '["'.$map['country'].'",'.$map['cases'].'],';
+                } ?>
+            ]);
+
+            var options = {
+                colorAxis: {colors: ['#D2E3FC', '#8AB4F8', '#4285F4', '#1967D2', '#174EA6', ]},
+                backgroundColor: '#2A323C',
+                legend: { textStyle: {color: '#000',fontSize: 14}},
+                displayMode: 'regions',
+            };
+
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+            chart.draw(data, options);
+        }
+    </script>
+</head>
