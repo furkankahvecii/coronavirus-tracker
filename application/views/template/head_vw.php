5a8447faca793244de40519bdf78626c40a74aea
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2020-04-05 02:35:33 +0300

    Create head_vw.php

---
+++ application/views/template/head_vw.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Corona Virus Tracker by Furkan KAHVECİ">
+    <meta name="keywords" content="Corona, COVID-19, Coronavirus, Coronavirus Tracker">
+    <meta name="author" content="Furkan KAHVECİ">
+    <meta http-equiv="X-UA-CompatibBle" content="IE=edge" />
+    <title><?php echo $title;?></title>
+    <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico">
+    <?php $this->load->view('template/css_vw');?>
+    
