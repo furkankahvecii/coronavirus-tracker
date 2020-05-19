2bceecc8d3e9d11fdfa85cb1bc3292c6c983f8e8
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-05-20 01:15:01 +0300

    Kıtaların gösterimi kaldırıldı.

--- application/views/coronavirus_vw.php
+++ application/views/coronavirus_vw.php
@@ -70,51 +70,9 @@
                         </div>
                     </div>
 
-
                     <div class="row">
                         <div class="col-sm-12 col-lg-12">
                             <div id="regions_div"> </div>
-                        </div>
-                    </div>
-
-
-                    <div class="row mt-3">
-                        <div class="col-lg-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="m-b-30 m-t-0">Statistics Continent</h4>
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="table-responsive">
-                                                <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Location</th>
-                                                            <th>Confirmed cases</th>
-                                                            <th>Deaths</th>
-                                                            <th>Recovered</th>
-                                                            <th>Total Tests</th>
-                                                            <th>Changes since last day</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php foreach($RESULT_DATA_CONTİNENT as $result){ ?>
-                                                        <tr>           
-                                                            <td><?php echo $result['country'];?></td>
-                                                            <td><?php echo number_format($result['cases']);?></td>
-                                                            <td><?php echo number_format($result['deaths']);?></td>
-                                                            <td><?php echo number_format($result['recovered']);?></td>
-                                                            <td><?php echo number_format($result['totalTests']);?></td>
-                                                            <td><?php echo number_format($result['todayCases']);?></td>
-                                                        </tr>
-                                                        <?php } ?>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
